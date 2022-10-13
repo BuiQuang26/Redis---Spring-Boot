a433c84c3b60664c8761c1e43e00dd00a2b198fd
QuangBui <qaker1035@gmail.com>
2022-10-13 08:52:26 +0700

    first commit

---
+++ src/test/java/vn/quangbui26/redis_spring_boot/RedisSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.quangbui26.redis_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
